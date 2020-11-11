5863567d40f56491e6115c3c902acf90db60ddb1
Brandon Casey <54991782+KaiYinfei@users.noreply.github.com>
2020-11-10 23:30:06 -0600

    Removed Debug Banner

--- battleship/lib/main.dart
+++ battleship/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Battleship',
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
             body: Container(
                 constraints: BoxConstraints.expand(),
