6917643738ef4ed889a2ac2ec0e7f119363f12a8
Brandon Casey <54991782+KaiYinfei@users.noreply.github.com>
2020-12-01 09:14:55 -0600

    Added tests for gameBoard

--- battleship/test/widget_test.dart
+++ battleship/test/widget_test.dart
@@ -1,30 +1,34 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:battleship/main.dart';
+import 'package:battleship/gameBoard.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  test('There is a ship in this position', () {
+    expect(gameBoard[0][0], 'occupied');});
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Attack an occupied space', () {
+    expect(_attackPosition(0, 0), 'hit');
   });
+
+  test('Attack an empty space', () {
+    expect(_attackPosition(3, 0), 'miss');
+  });
+
+  test('Attack an already attacked position', () {
+    expect(_attackPosition(3, 2), 'Invalid attack');
+  });
+}
+
+String _attackPosition(int x, int y){
+  if(gameBoard[x][y] == ''){
+    gameBoard[x][y] = 'miss';
+    return 'miss';
+  } else if(gameBoard[x][y] == 'miss'){
+    return 'Invalid attack';
+  } else if(gameBoard[x][y] == 'occupied') {
+    gameBoard[x][y] = 'hit';
+    return 'hit';
+  }
 }
